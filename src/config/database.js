ad5e4fc4dabd64583a1d7760713a33f910b270e4
Nguyễn Minh Thành <minhthanh5346@gmail.com>
2023-11-09 19:13:01 +0700

    Online Database (#8)
    * config files
    
    * config file and web routes
    
    * adding db4free application

--- src/config/database.js
+++ src/config/database.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection({
     port: process.env.DB_PORT || 3307,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: process.env.DB_NAME
 });
 
 module.exports = db;    
--- src/routes/web.js
+++ src/routes/web.js
@@ -7,20 +7,10 @@ const router = express.Router()
 
 // router.Method('/route' , handler    )
 router.get( '/' , getHomePage )
-
 router.post( '/create-user' , createUser )
 router.get('/abc' , getABC )
 router.get('/users' , getUser )
 
-// app.get("/users", (req, res) => {
-//     db.query("SELECT * FROM user", (err, results) => {
-//       if (err) {
-//         return res.status(500).json({ error: "Database error" });
-//       }
-//       console.log(">>>>>results = ", results);
-//       return res.status(200).json(results);
-//     });
-//   });
 
 module.exports = router; // export default
 
