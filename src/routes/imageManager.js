df1928b9a2a8fc14df3c0b4e3d56bcbdeb2bf3fd
nguyentaan <vantaan2002@gmail.com>
2023-11-20 16:18:24 +0700

    Edit Image

--- src/routes/imageManager.js
+++ src/routes/imageManager.js
@@ -12,6 +12,12 @@ router.post('/saveimages', imageController.upload.single('file'), imageControlle
 router.get('/userimage/:user_id', imageController.getImagesByUserId);
 router.get('/getAllImages',imageController.getAllImages);
 router.delete('/deleteAllImages',imageController.deleteAllImages);
+router.put(
+  "/getImage/:user_id/:imageID",
+  imageController.getImageByUserIdAndImageId
+);
+
+// router.put("./editImage", imageController.editImage);
 
 // router.get('/userimages/:user_id', imageController.getImagesByUserId);
 
