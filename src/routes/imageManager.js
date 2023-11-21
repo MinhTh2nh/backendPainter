daf4f013e1ea3427673906d9e03b2cf3c47f6c33
Nguyễn Minh Thành <minhthanh5346@gmail.com>
2023-11-22 01:52:21 +0700

    Thanhnew (#26)
    * merge from main (#24)
    
    * Thanhnew (#15)
    
    * update apiDoc
    
    * fix bug
    
    * Thanhnew (#16)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * Thanhnew (#17)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * Thanhnew (#18)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * update v3
    
    * Thanhnew (#19)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * update v3
    
    * update 2 route for image table
    
    * Thanhnew (#20)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * update v3
    
    * update 2 route for image table
    
    * update 2 route for image table
    
    * Thanhnew (#21)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * update v3
    
    * update 2 route for image table
    
    * update 2 route for image table
    
    * upload imageController
    
    * Thanhnew (#22)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * update v3
    
    * update 2 route for image table
    
    * update 2 route for image table
    
    * upload imageController
    
    * v0.0.1 of tokens and cookies
    
    * v0.2 authentication
    
    * Edit Image
    
    * add two function
    
    * ..
    
    * merge (#27)
    
    * Thanhnew (#15)
    
    * update apiDoc
    
    * fix bug
    
    * Thanhnew (#16)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * Thanhnew (#17)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * Thanhnew (#18)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * update v3
    
    * Thanhnew (#19)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * update v3
    
    * update 2 route for image table
    
    * Thanhnew (#20)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * update v3
    
    * update 2 route for image table
    
    * update 2 route for image table
    
    * Thanhnew (#21)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * update v3
    
    * update 2 route for image table
    
    * update 2 route for image table
    
    * upload imageController
    
    * Thanhnew (#22)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * update v3
    
    * update 2 route for image table
    
    * update 2 route for image table
    
    * upload imageController
    
    * Thanhnew (#25)
    
    * merge from main (#24)
    
    * Thanhnew (#15)
    
    * update apiDoc
    
    * fix bug
    
    * Thanhnew (#16)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * Thanhnew (#17)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * Thanhnew (#18)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * update v3
    
    * Thanhnew (#19)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * update v3
    
    * update 2 route for image table
    
    * Thanhnew (#20)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * update v3
    
    * update 2 route for image table
    
    * update 2 route for image table
    
    * Thanhnew (#21)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * update v3
    
    * update 2 route for image table
    
    * update 2 route for image table
    
    * upload imageController
    
    * Thanhnew (#22)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * update v3
    
    * update 2 route for image table
    
    * update 2 route for image table
    
    * upload imageController
    
    * v0.0.1 of tokens and cookies
    
    * v0.2 authentication
    
    * Edit Image
    
    * add two function
    
    ---------
    
    Co-authored-by: Nguyễn Minh Thành <minhthanh5346@gmail.com>
    
    ---------
    
    Co-authored-by: nguyentaan <101977467+nguyentaan@users.noreply.github.com>
    
    * update two functions
    
    * Fix login controller
    
    * update
    
    * update
    
    ---------
    
    Co-authored-by: nguyentaan <vantaan2002@gmail.com>
    Co-authored-by: nguyentaan <101977467+nguyentaan@users.noreply.github.com>
    Co-authored-by: Truong Thinh <mune@mune.moe>

--- src/routes/imageManager.js
+++ src/routes/imageManager.js
@@ -6,16 +6,11 @@ const router = express.Router();
 
 
 router.post('/saveimages', imageController.upload.single('file'), imageController.addImage);
-router.get('/userimage/:user_id', imageController.getImagesByUserId);
+router.get('/userimage/:email', imageController.getImagesByUserEmail);
 router.get('/getAllImages',imageController.getAllImages);
 router.delete('/deleteAllImages',imageController.deleteAllImages);
-router.get(
-  "/getImage/:user_id/:imageID",
-  imageController.getImageByUserIDAndImageID
-);
-router.put("/updateImage/:user_id/:imageID",imageController.updateImageByUserIDAndImageID);
-
-// router.put("./editImage", imageController.editImage);
+router.get("/getImage/:imageID", imageController.getImagesByImageId);
+router.put("/editImage/:imageID", imageController.updateImage);
 
 // router.get('/userimages/:user_id', imageController.getImagesByUserId);
 
