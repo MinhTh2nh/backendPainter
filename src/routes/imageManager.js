b6b4edba27eeb3d6a44cdd26aaf149a03dc8c1e9
nguyentaan <vantaan2002@gmail.com>
2023-11-21 22:39:01 +0700

    update two functions

--- src/routes/imageManager.js
+++ src/routes/imageManager.js
@@ -9,13 +9,8 @@ router.post('/saveimages', imageController.upload.single('file'), imageControlle
 router.get('/userimage/:user_id', imageController.getImagesByUserId);
 router.get('/getAllImages',imageController.getAllImages);
 router.delete('/deleteAllImages',imageController.deleteAllImages);
-router.get(
-  "/getImage/:user_id/:imageID",
-  imageController.getImageByUserIDAndImageID
-);
-router.put("/updateImage/:user_id/:imageID",imageController.updateImageByUserIDAndImageID);
-
-// router.put("./editImage", imageController.editImage);
+router.get("/getImage/:imageID", imageController.getImagesByImageId);
+router.put("/editImage/:imageID", imageController.updateImage);
 
 // router.get('/userimages/:user_id', imageController.getImagesByUserId);
 
