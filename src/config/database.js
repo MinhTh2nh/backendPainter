a0c2cbbee740efb0bccf97eb63908a4dd9c63e1a
nguyentaan <101977467+nguyentaan@users.noreply.github.com>
2023-12-06 20:29:00 +0700

    Thanhnew (#32)
    * merge from main (#24)
    
    * Thanhnew (#15)
    
    * update apiDoc
    
    * fix bug
    
    * Thanhnew (#16)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * Thanhnew (#17)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * Thanhnew (#18)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * update v3
    
    * Thanhnew (#19)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * update v3
    
    * update 2 route for image table
    
    * Thanhnew (#20)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * update v3
    
    * update 2 route for image table
    
    * update 2 route for image table
    
    * Thanhnew (#21)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * update v3
    
    * update 2 route for image table
    
    * update 2 route for image table
    
    * upload imageController
    
    * Thanhnew (#22)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * update v3
    
    * update 2 route for image table
    
    * update 2 route for image table
    
    * upload imageController
    
    * v0.0.1 of tokens and cookies
    
    * v0.2 authentication
    
    * Edit Image
    
    * add two function
    
    * ..
    
    * merge (#27)
    
    * Thanhnew (#15)
    
    * update apiDoc
    
    * fix bug
    
    * Thanhnew (#16)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * Thanhnew (#17)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * Thanhnew (#18)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * update v3
    
    * Thanhnew (#19)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * update v3
    
    * update 2 route for image table
    
    * Thanhnew (#20)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * update v3
    
    * update 2 route for image table
    
    * update 2 route for image table
    
    * Thanhnew (#21)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * update v3
    
    * update 2 route for image table
    
    * update 2 route for image table
    
    * upload imageController
    
    * Thanhnew (#22)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * update v3
    
    * update 2 route for image table
    
    * update 2 route for image table
    
    * upload imageController
    
    * Thanhnew (#25)
    
    * merge from main (#24)
    
    * Thanhnew (#15)
    
    * update apiDoc
    
    * fix bug
    
    * Thanhnew (#16)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * Thanhnew (#17)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * Thanhnew (#18)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * update v3
    
    * Thanhnew (#19)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * update v3
    
    * update 2 route for image table
    
    * Thanhnew (#20)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * update v3
    
    * update 2 route for image table
    
    * update 2 route for image table
    
    * Thanhnew (#21)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * update v3
    
    * update 2 route for image table
    
    * update 2 route for image table
    
    * upload imageController
    
    * Thanhnew (#22)
    
    * update apiDoc
    
    * fix bug
    
    * update
    
    * update v2
    
    * update
    
    * update v3
    
    * update 2 route for image table
    
    * update 2 route for image table
    
    * upload imageController
    
    * v0.0.1 of tokens and cookies
    
    * v0.2 authentication
    
    * Edit Image
    
    * add two function
    
    ---------
    
    Co-authored-by: Nguyễn Minh Thành <minhthanh5346@gmail.com>
    
    ---------
    
    Co-authored-by: nguyentaan <101977467+nguyentaan@users.noreply.github.com>
    
    * update two functions
    
    * Fix login controller
    
    * update
    
    * update
    
    * adding some routes
    
    * add controller delete image by id
    
    * update
    
    * Update .gitignore
    
    * update
    
    ---------
    
    Co-authored-by: Nguyễn Minh Thành <minhthanh5346@gmail.com>
    Co-authored-by: Truong Thinh <mune@mune.moe>

--- src/config/database.js
+++ src/config/database.js
@@ -7,9 +7,9 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  // waitForConnections: true,
+  // connectionLimit: 10,
+  // queueLimit: 0,
 });
 
 module.exports = db; // Export the promise-based pool
